2417a638463d35f06d05e58a877e87462a70e8ac
IgorGalimski <igorg@fm-games.com>
2022-02-26 21:55:43 +0200

    Add forgot password view

---
+++ Chat/ForgotPasswordViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ForgotPasswordViewController.swift
+//  Chat
+//
+//  Created by IgorGalimski on 26/02/2022.
+//
+
+import Foundation
+import UIKit
+
+class ForgotPasswordViewController : UIViewController
+{
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var resetPasswordButton: UIButton!
+}
